3687a6fe2703e960ee8e1e874f95919b8c43bf77
ProtonLT <logan.taveira@gmail.com>
2021-02-07 00:22:30 -0500

    change to brie theme

--- HackBU 2021/Properties/Resources.Designer.cs
+++ HackBU 2021/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace HackBU_2021.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Brie_01 {
+            get {
+                object obj = ResourceManager.GetObject("Brie_01", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
